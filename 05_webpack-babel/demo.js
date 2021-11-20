32e63c4adbcdd85df74bbb8482c77d9b39a46b8d
chenfengbin <1838272147@qq.com>
2021-11-20 14:50:36 +0800

    babel es6 > es5

---
+++ 05_webpack-babel/demo.js
@@ -0,0 +1,7 @@
+// es6代码
+const message = "Hello World";
+const names = ["abc", "cba", "nba"];
+
+names.forEach(item => console.log(item));
+
+// 使用babel工具
---
+++ 05_webpack-babel/dist/demo.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// es6代码
+var message = "Hello World";
+var names = ["abc", "cba", "nba"];
+names.forEach(function (item) {
+  return console.log(item);
+}); // 使用babel工具
